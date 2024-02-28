2f3e61fc510e6199cd5f182254a9aa5880476aef
Никита Мухаметдинов <muhametdinovn@mail.ru>
2024-02-28 16:13:11 +0600

    lesson_7 parser (NOT FINISHED)

---
+++ lesson_7/book_scraper/README.md
@@ -0,0 +1,3 @@
+you can start your first spider with:
+
+***scrapy genspider example example.com***
---
+++ lesson_7/book_scraper/book_scraper/spiders/labirint_ru.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+import scrapy
+
+
+class BookSpecs():
+    name: str
+    authors: str
+    price: int
+    isbn: str
+    publisher: str
+    numberOfPages: int
+    year: int
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = ["https://www.labirint.ru/books/571931/"]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f"labirint-{page}.html"
+        Path(filename).write_bytes(response.body)
+
+        for detail in response.css("div.product-description"):
+
+            author = detail.css("a.analytics-click-js::text").get()
+            price = detail.css("span.buying-priceold-val-number::text").get()
+            numberOfPages = detail.css("div.pages2::text").get() #требуется регулярка чтобы страницы вытащить из текста
+            isbn = detail.css("div.isbn::text").get() #требуется регулярка чтобы вытащить номер ISBN
+            publisher = detail.css("div.publisher a.analytics-click-js::text").get()
+            print(dict(
+                author=author,
+                price=price,
+                numberOfPages=numberOfPages,
+                isbn=isbn,
+                publisher=publisher
+            ))
+        self.log(f"Saved file {filename}")
